82067b53c7e2e7ae4fd57e3027cc16a7fe5a685a
Jayaprakash Pattanaik <jpattanaik1983@gmail.com>
2023-02-23 11:44:22 +0530

    Initial commit

---
+++ Qn_1480.py
@@ -0,0 +1,16 @@
+# 
+# RUNNING SUM of 1D Array
+
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+# Return the running sum of nums.
+
+nums = []
+while True:
+    a = input(f'kindly provide the list to calculate the running sum...(done to Finalize): ')  
+    nums.append(a)
+    if a == "done":
+        break
+    else:
+        print(sfjvahgjkasfdgkjn)
+
+print(nums)
